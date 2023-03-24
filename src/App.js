7d3fa55db65e4b71d4e3ac55f61ee51f20784e18
Kevin Callon <kevincallon@gmail.com>
2023-03-24 12:30:34 +0100

    replace unmaintained react-google-maps library with newer, maintained react-google-maps/api library

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from "react";
 import { Map } from "./Map";
-import { withScriptjs, withGoogleMap } from "react-google-maps";
 import { Header } from "./Header";
-const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
   return (
     <div id="app-container">
       <Header />
       <div id='map-container'>
-        <WrappedMap
-          id="wrappedmap"
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLE_MAPS_API_KEY}`}
-          loadingElement={<div style={{ height: "100%" }} />}
-          containerElement={
-            <div
-              style={{
-                height: "100%",
-                border: "5px #6e769e solid"
-              }}
-            />
-          }
-          mapElement={<div style={{ height: "100%" }} />}
-        />
+        <Map />
       </div>
     </div>
   );
--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-import { GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import React, { memo, useEffect, useState } from "react";
+import { GoogleMap, Marker, InfoWindow, LoadScript } from '@react-google-maps/api';
 import customMapStyle from "./custommapstyle";
 import skatespots from './skatespots';
+const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
-export function Map() {
+export const Map = memo(() => {
   const [selectedSpot, setSelectedSpot] = useState(null);
   const [largerImage, setLargerImage] = useState(null);
+
+  const containerStyle = {
+    height: "100%",
+    border: "5px #6e769e solid"
+  };
 
   const closeInfoWindow = () => {
     setSelectedSpot(null);
@@ -16,52 +22,59 @@ export function Map() {
     setLargerImage(prev => !prev && window.innerWidth <= 1800 ? "larger" : null);
   };
 
+  useEffect(() => console.log('Hi there, thanks for inspecting my app. If you see an "ERR_BLOCKED_BY_CLIENT" error in the console, it is likely caused by your adblocker.'), []);
+
   return (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 52.520008, lng: 13.404954 }}
-      defaultOptions={{ styles: customMapStyle }}
-      onClick={closeInfoWindow}
+    <LoadScript
+      googleMapsApiKey={GOOGLE_MAPS_API_KEY}
     >
-      {skatespots.map(spot => (
-        <Marker
-          key={spot.id}
-          position={{ lat: parseFloat(spot.lat), lng: parseFloat(spot.lng) }}
-          onClick={() => setSelectedSpot(spot)}
-        />
-      ))}
-      {selectedSpot && (
-        <InfoWindow
-          position={{ lat: parseFloat(selectedSpot.lat), lng: parseFloat(selectedSpot.lng) }}
-          onCloseClick={closeInfoWindow}
-        >
-          <div id="info-window" className={largerImage}>
-            <div id="info-window-name">{selectedSpot.name}</div>
-            {selectedSpot.img && (
-              <img
-                id="info-window-img"
-                onClick={handleImgClick}
-                src={selectedSpot.img}
-                className={largerImage}
-                alt="skate spot"
-              />
-            )}
-            <div id="info-window-description">
-              {selectedSpot.description}
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={{ lat: 52.520008, lng: 13.404954 }}
+        zoom={12}
+        options={{ styles: customMapStyle }}
+        onClick={closeInfoWindow}
+      >
+        {skatespots.map(spot => (
+          <Marker
+            key={spot.id}
+            position={{ lat: parseFloat(spot.lat), lng: parseFloat(spot.lng) }}
+            onClick={() => setSelectedSpot(spot)}
+          />
+        ))}
+        {selectedSpot && (
+          <InfoWindow
+            position={{ lat: parseFloat(selectedSpot.lat), lng: parseFloat(selectedSpot.lng) }}
+            onCloseClick={closeInfoWindow}
+          >
+            <div id="info-window" className={largerImage}>
+              <div id="info-window-name">{selectedSpot.name}</div>
+              {selectedSpot.img && (
+                <img
+                  id="info-window-img"
+                  onClick={handleImgClick}
+                  src={selectedSpot.img}
+                  className={largerImage}
+                  alt="skate spot"
+                />
+              )}
+              <div id="info-window-description">
+                {selectedSpot.description}
+              </div>
+              <div id="info-window-address-directions-container">
+                <div id="info-window-address">
+                  {selectedSpot.address}
+                </div>{" "}
+                <a target={"_blank"} rel="noopener noreferrer" href={selectedSpot.directions}>
+                  <button id="info-window-directions">
+                    Directions
+                  </button>
+                </a>
+              </div>
             </div>
-            <div id="info-window-address-directions-container">
-              <div id="info-window-address">
-                {selectedSpot.address}
-              </div>{" "}
-              <a target={"_blank"} rel="noopener noreferrer" href={selectedSpot.directions}>
-                <button id="info-window-directions">
-                  Directions
-                </button>
-              </a>
-            </div>
-          </div>
-        </InfoWindow>
-      )}
-    </GoogleMap>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    </LoadScript>
   );
-}
+});
