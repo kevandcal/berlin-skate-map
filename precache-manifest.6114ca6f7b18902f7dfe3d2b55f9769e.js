94369bc14e2ebeeb244aba82ba507a1bfd5f4e0e
Kevin Callon <kevincallon@gmail.com>
2023-03-24 09:54:35 +0100

    Updates

---
+++ precache-manifest.6114ca6f7b18902f7dfe3d2b55f9769e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea79c5a1742c0ffe62d07f7362b4873f",
+    "url": "/berlin-skate-map/index.html"
+  },
+  {
+    "revision": "2578f17649f4a7a0418e",
+    "url": "/berlin-skate-map/static/css/main.c50edcb1.chunk.css"
+  },
+  {
+    "revision": "2feb28ffa37882ed36d7",
+    "url": "/berlin-skate-map/static/js/2.77e53238.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/berlin-skate-map/static/js/2.77e53238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2578f17649f4a7a0418e",
+    "url": "/berlin-skate-map/static/js/main.643fa0cb.chunk.js"
+  },
+  {
+    "revision": "ce10f9b043fac274c5c3",
+    "url": "/berlin-skate-map/static/js/runtime-main.629cd12b.js"
+  },
+  {
+    "revision": "8d75d78bc087e9bd2eca639979ad139c",
+    "url": "/berlin-skate-map/static/media/albert-nyberg-kulturforum.8d75d78b.gif"
+  },
+  {
+    "revision": "155f754e255855f7e8dfca19f022617f",
+    "url": "/berlin-skate-map/static/media/berlin-skatehalle.155f754e.jpeg"
+  },
+  {
+    "revision": "63c4195d185c56c3ea6e942bf898f0ec",
+    "url": "/berlin-skate-map/static/media/dog-shit-spot.63c4195d.jpg"
+  },
+  {
+    "revision": "d1eb72ed9a9110973dcc39e7990d2262",
+    "url": "/berlin-skate-map/static/media/east-side-gallery.d1eb72ed.jpeg"
+  },
+  {
+    "revision": "c9644ed06d0a154ad09f55025f733666",
+    "url": "/berlin-skate-map/static/media/heidelberger-platz.c9644ed0.jpeg"
+  },
+  {
+    "revision": "7e233303c28aef49477979ebdf43296c",
+    "url": "/berlin-skate-map/static/media/kleistpark-tobi.7e233303.jpg"
+  },
+  {
+    "revision": "24afd7a7ad47eb895d3b491247c48855",
+    "url": "/berlin-skate-map/static/media/luan-oliveira-berlin-baenke.24afd7a7.gif"
+  }
+]);
